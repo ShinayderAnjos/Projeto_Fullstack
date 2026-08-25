abbdd434cb32f247e536dc8b1c6dedbda18de027
Shinayder Anjos <anjosshinayder@gmail.com>
2026-08-25 14:15:01 -0300

    Primeiro commit

---
+++ src/main/java/br/ueg/trindade/shinayder_projeto_fullstack/ShinayderProjetoFullstackApplication.java
@@ -0,0 +1,13 @@
+package br.ueg.trindade.shinayder_projeto_fullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShinayderProjetoFullstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShinayderProjetoFullstackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/ueg/trindade/shinayder_projeto_fullstack/ShinayderProjetoFullstackApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ueg.trindade.shinayder_projeto_fullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShinayderProjetoFullstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
